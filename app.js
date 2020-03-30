6704e654f8b8bb306ff3b79949c81c93ed828d5c
Satoru Shimono <michael513.sato@gmail.com>
2020-03-30 21:44:24 +0900

    import from node_modules for mdbootstrap

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
